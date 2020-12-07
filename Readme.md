bdf35e6cd3d25935dce577b68466ee268d0861aa
Bruce Wang <brucewangsg@icloud.com>
2020-12-07 08:49:48 +0800

    Add docs for db migration

--- Readme.md
+++ Readme.md
@@ -4,3 +4,11 @@
 
 The app uses postgresql to store data. Interfacing with db from go app is done using gorm. We use gorm mainly for its automigrate and basic create, update and find individual records.
 
+### migrating database
+
+```
+go build -o bin/migratedb cmds/config.go cmds/migrate.go
+DB_HOST={host} DB_USER={user} DB_NAME={dbname} DB_PORT={dbport} DB_PASS={yourdbpass} bin/migratedb
+```
+
+replace env variables accordingly
--- cmds/migrate.go
+++ cmds/migrate.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/brucewangsg/locsvcexercise/authsvc"
 	"github.com/brucewangsg/locsvcexercise/coresvc"
 )
@@ -9,8 +11,10 @@ func main() {
 	config := newAppConfig()
 	db := newAppDBPool(config)
 
+	fmt.Println("Migrate users table")
 	db.AutoMigrate(&authsvc.User{})
 	db.Exec("CREATE UNIQUE INDEX users_email ON users (email)")
 
+	fmt.Println("Migrate locations table")
 	db.AutoMigrate(&coresvc.Location{})
 }
